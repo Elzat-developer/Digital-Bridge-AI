4718e903e711bf8b421f8392a54f95af85763f31
Elzat-developer <fergenson.2020@gmail.com>
2025-11-08 12:40:16 +0500

    Аутентификация готова

---
+++ src/main/java/db/ai/digitalbridgeai/controller/AdminPanelController.java
@@ -0,0 +1,22 @@
+package db.ai.digitalbridgeai.controller;
+
+import db.ai.digitalbridgeai.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/admin")
+@RequiredArgsConstructor
+public class AdminPanelController {
+//    @GetMapping("/users")
+//    public ResponseEntity<List<User>> getUserList(){
+//        ad
+//        return users;
+//    }
+}
--- src/test/java/db/ai/digitalbridgeai/DigitalBridgeAiApplicationTests.java
+++ src/test/java/db/ai/digitalbridgeai/DigitalBridgeAiApplicationTests.java
@@ -1,9 +1,13 @@
 package db.ai.digitalbridgeai;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@AutoConfigureMockMvc(addFilters = false)
+@ActiveProfiles("test")
 class DigitalBridgeAiApplicationTests {
 
     @Test
