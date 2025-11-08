a7173a68af9d0bad67f72ae79f29fea8b63e2389
Elzat-developer <fergenson.2020@gmail.com>
2025-11-08 11:11:01 +0500

    first commit

---
+++ src/main/java/db/ai/digitalbridgeai/DigitalBridgeAiApplication.java
@@ -0,0 +1,13 @@
+package db.ai.digitalbridgeai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalBridgeAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalBridgeAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/db/ai/digitalbridgeai/DigitalBridgeAiApplicationTests.java
@@ -0,0 +1,13 @@
+package db.ai.digitalbridgeai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalBridgeAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
